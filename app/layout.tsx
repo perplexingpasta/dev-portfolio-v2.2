24289b1aed3d7fa8f72e27d933278f116cfbc1d9
Rishabh <github@aperta.anonaddy.com>
2024-07-27 09:11:11 +0530

    implemented posthog

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,68 @@
+// import { SpeedInsights } from "@vercel/speed-insights/next";
+// import { Analytics } from "@vercel/analytics/react";
+// import type { Metadata } from "next";
+// import { Montserrat, Lexend_Deca } from "next/font/google";
+// import "./globals.css";
+// import { ThemeProvider } from "./provider";
+
+// const montserrat = Montserrat({
+//   subsets: ["latin"],
+//   display: "swap",
+// });
+
+// const lexenddeca = Lexend_Deca({
+//   subsets: ["latin"],
+//   display: "swap",
+// });
+
+// export const metadata: Metadata = {
+//   title:
+//     "Céleste Consulting | Helping Dermatologists with Consistent, Predictable & Loyal Patient Acquisition",
+//   description:
+//     "We help Dermatologists with consistent, predictable & loyal patient acquisition. We get dermatologists more patients and help them grow their practice online by running paid ads on facebook and google and professional website development using nextjs. We also help set up their google business profile and increase google reviews on google maps of their clinic.",
+//   other: {
+//     "twitter:image": "/images/ogmeta.png",
+//     "twitter:card": "céleste consulting",
+//     "og:url": "https://www.celesteconsulting.in/",
+//     "og:image": "/images/ogmeta.png",
+//     "og:type": "website",
+//   },
+//   icons: {
+//     icon: ["/images/favicon.ico"],
+//     apple: ["/images/apple-touch-icon.png"],
+//     shortcut: ["/images/apple-touch-icon.png"],
+//   },
+// };
+
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <html lang="en">
+//       <body className={montserrat.className}>
+//         <ThemeProvider
+//           attribute="class"
+//           defaultTheme="dark"
+//           enableSystem
+//           disableTransitionOnChange
+//         >
+//           {children}
+//           <Analytics />
+//           <SpeedInsights />
+//         </ThemeProvider>
+//       </body>
+//     </html>
+//   );
+// }
+
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Montserrat, Lexend_Deca } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "./provider";
+import { CombinedProvider } from "./provider";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -42,7 +101,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={montserrat.className}>
-        <ThemeProvider
+        <CombinedProvider
           attribute="class"
           defaultTheme="dark"
           enableSystem
@@ -51,8 +110,9 @@ export default function RootLayout({
           {children}
           <Analytics />
           <SpeedInsights />
-        </ThemeProvider>
+        </CombinedProvider>
       </body>
     </html>
   );
 }
+
--- app/provider.tsx
+++ app/provider.tsx
@@ -1,9 +1,39 @@
-"use client";
+// "use client";
 
-import * as React from "react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes/dist/types";
+// import * as React from "react";
+// import { ThemeProvider as NextThemesProvider } from "next-themes";
+// import { type ThemeProviderProps } from "next-themes/dist/types";
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+// export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+//   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+// }
+
+// app/providers.js
+'use client'
+
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
+import * as React from 'react'
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
+import { type ThemeProviderProps } from 'next-themes/dist/types'
+
+const POSTHOG_KEY = process.env.NEXT_PUBLIC_POSTHOG_KEY || '';
+const POSTHOG_HOST = process.env.NEXT_PUBLIC_POSTHOG_HOST || '';
+
+if (typeof window !== 'undefined') {
+  posthog.init(POSTHOG_KEY, {
+    api_host: POSTHOG_HOST,
+    person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+  })
+}
+type CombinedProviderProps = ThemeProviderProps & {
+  children: React.ReactNode;
+}
+
+export function CombinedProvider({ children, ...props }: CombinedProviderProps) {
+  return (
+    <PostHogProvider client={posthog}>
+      <NextThemesProvider {...props}>{children}</NextThemesProvider>
+    </PostHogProvider>
+  )
 }
